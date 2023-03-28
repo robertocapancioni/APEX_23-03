ba9369cae439be36f0c0f86b25a65878d3fadf4e
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-03-28 06:49:17 +0200

    Update d03_classifica_ft.sql

--- D03/d03_classifica_ft.sql
+++ D03/d03_classifica_ft.sql
@@ -4,4 +4,41 @@ select classifica,
        settore,
        revenue_2020,
        anno_inizio
+  from d03_classifica_ft;
+  
+select classifica,
+       nome,
+       nazione,
+       settore,
+       revenue_2020,
+       anno_inizio
   from d03_classifica_ft
+ where nazione = 'Italy';
+ 
+  select classifica,
+         nome,
+         nazione,
+         settore,
+         revenue_2020,
+         anno_inizio
+    from d03_classifica_ft
+   where nazione = 'Italy'
+order by classifica
+   fetch next 3 rows only; 
+
+   select nazione,
+          count(*) as numero
+     from d03_classifica_ft
+ group by nazione;
+ 
+    select nazione,
+          count(*) as numero,
+          sum(revenue_2020) as tot_revenue_2020
+     from d03_classifica_ft
+ group by nazione;
+  
+   select count(*) as numero,
+          count(web) as numero_web,
+          count(distinct nazione) numero_nazioni
+     from d03_classifica_ft;
+
