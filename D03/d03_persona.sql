c83292cc79a900e158962815919b43513f082ed1
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-03-28 12:03:55 +0200

    Create d03_persona.sql

---
+++ D03/d03_persona.sql
@@ -0,0 +1,11 @@
+persona 
+  nome vc50 /nn 
+  cognome vc50 /nn
+  data nascita date
+  email
+  attivo_yn /default  N
+  documento clob
+  telefono vc255
+  foto file
+
+#settings = { prefix: d03,drop: true, auditCols: true}
