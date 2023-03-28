ffbdb15af96dc6c3e401804dc04a7233ecbbd82a
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-03-28 07:20:36 +0200

    Update d03_classifica_ft.sql

--- D03/d03_classifica_ft.sql
+++ D03/d03_classifica_ft.sql
@@ -42,3 +42,43 @@ order by classifica
           count(distinct nazione) numero_nazioni
      from d03_classifica_ft;
 
+    select nazione,
+           count(*) as numero,
+           sum(revenue_2020) as tot_revenue_2020,
+           avg(revenue_2020) as media_revenue_2020
+      from d03_classifica_ft
+  group by nazione
+  order by tot_revenue_2020 desc
+fetch next 3 rows only;
+
+    select nazione,
+           count(*) as numero,
+           sum(revenue_2020) as tot_revenue_2020,
+           avg(revenue_2020) as media_revenue_2020
+      from d03_classifica_ft
+  group by nazione
+  order by sum(revenue_2020) desc
+fetch next 3 rows only;
+
+select nazione,
+           count(*) as numero,
+           sum(revenue_2020) as tot_revenue_2020,
+           avg(revenue_2020) as media_revenue_2020
+      from d03_classifica_ft
+  group by nazione
+    having sum(revenue_2020) >180000000
+  order by tot_revenue_2020 desc;
+
+    select nazione,
+           count(*) as numero,
+           count(distinct NOME) num_aziende,
+           sum(revenue_2020) as tot_revenue_2020,
+           max(revenue_2020) as max_revenue_2020,
+           round(avg(revenue_2020),2) as media_revenue_2020
+      from d03_classifica_ft
+     where settore = 'Technology'
+  group by nazione
+    having sum(revenue_2020) >180000000
+     order by tot_revenue_2020 desc
+     fetch next 3 rows only;
+  order by tot_revenue_2020 desc;
