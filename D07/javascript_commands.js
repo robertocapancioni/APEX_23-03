35c7c9a68e46d314973bd708b5f8946bbf1d1d04
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-04-11 16:11:53 +0200

    Create javascript_commands.js

---
+++ D07/javascript_commands.js
@@ -0,0 +1,24 @@
+apex.item( "P5_CATEGORIA" ).getValue();
+$( "#P5_CATEGORIA" ).val();
+$v( "P5_CATEGORIA" );
+$x("P5_CATEGORIA").value;
+
+
+apex.item( "P5_CATEGORIA" ).setValue('Vikas');
+$x("P5_CATEGORIA").value='Vikas';
+$("#P5_CATEGORIA" ).val('Vikas'); // ??
+$s('P5_CATEGORIA','Vikas');   // ??
+
+$("#P5_CATEGORIA_LABEL").text(); //to get lable text
+
+$("#P5_CATEGORIA_LABEL").text('Vikas Pandey'); //to Set lable text
+
+
+apex.item( "P4_ID" ).setValue('1');
+apex.item( "P4_ID" ).getValue();
+
+apex.submit({request:"SAVE",
+             set:{"P4_ID":"1"}
+            });
+
+apex.submit({request:"DELETE"});
