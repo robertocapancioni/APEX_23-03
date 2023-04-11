a13bee4f76f98de4d5083b7bcc4790c066e59b6f
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-04-11 11:34:14 +0200

    Update d07_6_documento_tvw.sql

--- D07/d07_6_documento_tvw.sql
+++ D07/d07_6_documento_tvw.sql
@@ -13,12 +13,12 @@ instead of insert on d07_documento_tvw
 for each row 
 begin 
         d07_documento_api.insert_row (
-            p_id                          => null,
-            p_tipo_documento_id           => null,
-            p_codice                      => null,
-            p_data_documento              => null,
-            p_firma_yn                    => null,
-            p_categoria                   => null
+            p_id                          => :new.id,                          
+            p_tipo_documento_id           => :new.tipo_documento_id,           
+            p_codice                      => :new.codice,                      
+            p_data_documento              => :new.data_documento,              
+            p_firma_yn                    => :new.firma_yn,                    
+            p_categoria                   => :new.categoria                   
             );
 end; 
 /
@@ -28,12 +28,12 @@ instead of update on d07_documento_tvw
 for each row 
 begin 
         d07_documento_api.update_row (
-            p_id                          => null,
-            p_tipo_documento_id           => null,
-            p_codice                      => null,
-            p_data_documento              => null,
-            p_firma_yn                    => null,
-            p_categoria                   => null
+            p_id                          => :new.id,                          
+            p_tipo_documento_id           => :new.tipo_documento_id,           
+            p_codice                      => :new.codice,                      
+            p_data_documento              => :new.data_documento,              
+            p_firma_yn                    => :new.firma_yn,                    
+            p_categoria                   => :new.categoria   
             );
 end; 
 /
@@ -43,7 +43,7 @@ instead of delete on d07_documento_tvw
 for each row 
 begin 
         d07_documento_api.delete_row (
-        p_id                           => null
+        p_id                           => :new.id
     );
 end; 
 /
