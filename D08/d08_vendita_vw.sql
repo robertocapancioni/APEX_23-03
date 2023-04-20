8de3a7256b38f3c652cf4b2e12ff5bf376051659
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-04-20 06:05:23 +0200

    Update d08_vendita_vw.sql

--- D08/d08_vendita_vw.sql
+++ D08/d08_vendita_vw.sql
@@ -2,6 +2,7 @@ create or replace view d08_vendita_vw as
 select v.id,
        v.cliente_id,
        c.cliente,
+       c.zona,
        v.prodotto_id,
        p.prodotto,
        p.tipo_prodotto_id,
@@ -13,5 +14,7 @@ select v.id,
   join d08_cliente c on v.cliente_id = c.id
   join d08_prodotto p on v.prodotto_id = p.id
   join d08_tipo_prodotto tp on p.tipo_prodotto_id = tp.id;
+
+
 
 
