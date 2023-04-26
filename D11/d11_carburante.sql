9d5ec06086d5707999f389595d9ee4558b954d93
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-04-26 22:11:20 +0200

    Update d11_carburante.sql

--- D11/d11_carburante.sql
+++ D11/d11_carburante.sql
@@ -5,3 +5,22 @@ select targa,
     from d11_carburante
 order by targa,data;
 
+select targa,
+         data,
+         litri,
+         km_progr,
+         lead(km_progr) over(partition by targa 
+                                 order by data  ) km_progr_successivi
+    from d11_carburante
+order by targa,data;
+
+select targa,
+         data,
+         litri,
+         km_progr,
+         lag(km_progr) over(partition by targa 
+                                 order by data  ) km_progr_precedenti,
+         lead(km_progr) over(partition by targa 
+                                 order by data  ) km_progr_successivi
+    from d11_carburante
+order by targa,data;
