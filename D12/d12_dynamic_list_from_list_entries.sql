90840fa838510928007f16d2428953fb6e93fd2b
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-05-02 11:15:40 +0200

    Update d12_dynamic_list_from_list_entries.sql

--- D12/d12_dynamic_list_from_list_entries.sql
+++ D12/d12_dynamic_list_from_list_entries.sql
@@ -13,3 +13,20 @@ select null "level",
 --   and list_name=''
 --   and parent_entry_text=''
  order by display_sequence
+
+
+select null "level",
+  ENTRY_TEXT label,
+  ENTRY_TARGET target,
+  ENTRY_TEXT image_alt_attribute,
+  ENTRY_IMAGE CARD_INITIALS,
+  '<img src="'||  ENTRY_IMAGE|| '"/>'  attribute3,
+  application_id,
+  application_name,
+  list_name,
+  parent_entry_text
+  from APEX_APPLICATION_LIST_ENTRIES 
+ where application_id =:APP_ID
+   and list_name='Menu di navigazione'
+   and parent_entry_text='Vendite'
+ order by display_sequence
