9d43eefdc47d0ab8d8504b4ee70cf2dbe65a001b
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-05-02 07:45:45 +0200

    Update d12_UNPIVOT_vendita.sql

--- D12/d12_UNPIVOT_vendita.sql
+++ D12/d12_UNPIVOT_vendita.sql
@@ -16,4 +16,40 @@ UNPIVOT [INCLUDE | EXCLUDE NULLS]
 )
 ORDER BY unpivot_colonna1, …;
 
+select * 
+  from
+(
+ select tipo_prodotto,
+        nord_quantita,
+        centro_quantita,
+        sud_quantita 
+   from d08_vendita_pivot_vw
+)
+unpivot(
+    quantita
+    for zona 
+    in (nord_quantita as 'NORD',
+        centro_quantita as 'CENTRO',
+        sud_quantita as 'SUD')
+);
 
+select * from
+(
+ select tipo_prodotto,
+        nord_quantita,
+        centro_quantita,
+        sud_quantita,
+        nord_valore,
+        centro_valore,
+        sud_valore 
+  from d08_vendita_pivot_vw
+)
+unpivot(
+    (quantita,valore)
+    for zona 
+    in (
+        (nord_quantita,nord_valore) as 'NORD',
+        (centro_quantita,centro_valore) as 'CENTRO',
+        (sud_quantita,sud_valore) as 'SUD'
+        )
+);
