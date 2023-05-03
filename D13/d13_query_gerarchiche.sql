216d925cff238a3b7af234fd7c38b3c8e4a3ea9f
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-05-04 00:57:47 +0200

    Update d13_query_gerarchiche.sql

--- D13/d13_query_gerarchiche.sql
+++ D13/d13_query_gerarchiche.sql
@@ -2,6 +2,154 @@ select empno,
        ename,
        job,
        mgr,
+       hiredate,
+       sal
+  from emp;
+
+select empno,
+       ename,
+       job,
+       mgr,
+       hiredate,
+       sal
+  from emp
+  start with mgr is null
+  connect by prior empno = mgr;
+
+select empno,
+       ename,
+       job,
+       mgr,
+       hiredate,
+       sal,
+       level
+  from emp
+  start with mgr is null
+  connect by prior empno = mgr
+  order siblings by ename;
+
+select empno,
+       ename,
+       job,
+       mgr,
+       prior ename mgr_ename,
+       prior job mgr_job,
+       hiredate,
+       sal,
+       level
+  from emp
+  start with mgr is null
+  connect by prior empno = mgr
+  order siblings by ename;
+  
+select empno,
+       ename,
+       job,
+       mgr,
+       prior ename mgr_ename,
+       prior job mgr_job,
+       connect_by_root empno root_empno,
+       hiredate,
+       sal,
+       level
+  from emp
+  start with mgr is null
+  connect by prior empno = mgr
+  order siblings by ename;
+
+select empno,
+       ename,
+       job,
+       mgr,
+       prior ename mgr_ename,
+       prior job mgr_job,
+       connect_by_root empno root_empno,
+       hiredate,
+       sal,
+       level,
+       SYS_CONNECT_BY_PATH(ename,'/') path
+  from emp
+  start with mgr is null
+  connect by prior empno = mgr
+  order siblings by ename;
+  
+select empno,
+       ename,
+       job,
+       mgr,
+       prior ename mgr_ename,
+       prior job mgr_job,
+       connect_by_root empno root_empno,
+       hiredate,
+       sal,
+       level,
+       SYS_CONNECT_BY_PATH(ename,'/') path,
+       CONNECT_BY_ISLEAF leaf
+  from emp
+  start with mgr is null
+  connect by prior empno = mgr
+  order siblings by ename;
+  
+ update emp set mgr = 7566 where empno=7839
+ 
+ select empno,
+       ename,
+       job,
+       mgr,
+       prior ename mgr_ename,
+       prior job mgr_job,
+       connect_by_root empno root_empno,
+       hiredate,
+       sal,
+       level,
+       SYS_CONNECT_BY_PATH(ename,'/') path,
+       CONNECT_BY_ISLEAF leaf
+  from emp
+  start with empno = 7839
+  connect by prior empno = mgr
+  order siblings by ename;
+  
+  select empno,
+       ename,
+       job,
+       mgr,
+       prior ename mgr_ename,
+       prior job mgr_job,
+       connect_by_root empno root_empno,
+       hiredate,
+       sal,
+       level,
+       SYS_CONNECT_BY_PATH(ename,'/') path,
+       CONNECT_BY_ISLEAF leaf
+  from emp
+  start with empno = 7839
+  connect by nocycle prior empno = mgr
+  order siblings by ename;
+ 
+ select empno,
+       ename,
+       job,
+       mgr,
+       prior ename mgr_ename,
+       prior job mgr_job,
+       connect_by_root empno root_empno,
+       hiredate,
+       sal,
+       level,
+       SYS_CONNECT_BY_PATH(ename,'/') path,
+       CONNECT_BY_ISLEAF leaf,
+       CONNECT_BY_ISCYCLE cycle
+  from emp
+  start with empno = 7839
+  connect by nocycle prior empno = mgr
+  order siblings by ename;
+  
+ update emp set mgr = null where empno=7839;  
+ 
+select empno,
+       ename,
+       job,
+       mgr,
        prior ename mgr_ename,
        prior job mgr_job,
        connect_by_root empno root_empno,
@@ -13,7 +161,7 @@ select empno,
        --CONNECT_BY_ISCYCLE cycle,
        CONNECT_BY_ISLEAF leaf
   from emp
- start with empno =7839
+ start with mgr is null
  connect by -- nocycle 
    prior empno = mgr
- order siblings by ename
+ order siblings by ename;
