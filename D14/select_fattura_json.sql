48b17e9741417f0f0f4faae89eeb7fe3e3e94bd8
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-05-09 06:51:35 +0200

    Update select_fattura_json.sql

--- D14/select_fattura_json.sql
+++ D14/select_fattura_json.sql
@@ -32,4 +32,81 @@ select *
 select json_object(*) righe_dettaglio 
   from d12_fattura_dettaglio;
 
+select  
+json_arrayagg(riga) riga
+from d12_fattura_dettaglio;
+
+select  
+json_arrayagg(riga 
+              order by riga
+              ) riga
+from d12_fattura_dettaglio;
+
+select  
+json_arrayagg(riga 
+              order by riga
+              returning clob) riga
+from d12_fattura_dettaglio;
+
+
+select  
+FATTURA_TESTATA_ID,
+json_arrayagg(riga 
+              order by riga
+              returning clob) riga
+from d12_fattura_dettaglio
+group by FATTURA_TESTATA_ID;
+
+select  
+FATTURA_TESTATA_ID,
+json_arrayagg(json_object(riga) 
+              order by riga
+              returning clob) riga
+from d12_fattura_dettaglio
+group by FATTURA_TESTATA_ID;
+
+select  
+FATTURA_TESTATA_ID,
+json_arrayagg(json_object(id,
+                          fattura_testata_id,
+                          riga,
+                          key 'DESCR' is descrizione, 
+                          quantita,
+                        importo 
+                          ) 
+              order by riga
+              returning clob) riga
+from d12_fattura_dettaglio
+group by FATTURA_TESTATA_ID;
+
+select json_object(
+                   t.id,
+                   t.cliente_id,
+                   c.ragione_sociale,
+                   t.anno,
+                   t.numero,
+                   t.descrizione,
+                   t.data,
+                   'righe  '           is (
+                                            select  
+                                            json_arrayagg(json_object(d.id,
+                                                                      d.fattura_testata_id,
+                                                                      d.riga,
+                                                                      d.descrizione, 
+                                                                      d.quantita,
+                                                                      d.importo 
+                                                                      ) 
+                                                        order by riga
+                                                        returning clob) riga
+                                            from d12_fattura_dettaglio d
+                                            where d.fattura_testata_id = t.id)
+                                         ) fattura
+  from d12_fattura_testata t
+  join d12_cliente c on t.cliente_id = c.id
+
+
+
+
+
+
 
