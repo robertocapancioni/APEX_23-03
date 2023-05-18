995d6d17d9d870b7b09687745438c4946dcdcb6c
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-05-18 08:15:12 +0200

    Update d17_apex_web_service.sql

--- D17/d17_apex_web_service.sql
+++ D17/d17_apex_web_service.sql
@@ -3,7 +3,7 @@ https://docs.oracle.com/en/database/oracle/apex/23.1/aeapi/MAKE_REST_REQUEST-Fun
 
 select 
 apex_web_service.make_rest_request(
-    p_url => 'https://g0a16906b023bfa-odcecmi.adb.eu-frankfurt-1.oraclecloudapps.com/ords/usrx00/d16_vendita/',
+    p_url => 'https://g0a16906b023bfa-odcecmi.adb.eu-frankfurt-1.oraclecloudapps.com/ords/usrx00/d17_vendita/',
     p_http_method => 'GET'
    ) vendita
 from dual;
@@ -12,7 +12,7 @@ from dual;
 select * 
   from json_table(
 apex_web_service.make_rest_request(
-    p_url => 'https://g0a16906b023bfa-odcecmi.adb.eu-frankfurt-1.oraclecloudapps.com/ords/usrx00/d16_vendita/',
+    p_url => 'https://g0a16906b023bfa-odcecmi.adb.eu-frankfurt-1.oraclecloudapps.com/ords/usrx00/d17_vendita/',
     p_http_method => 'GET'
    ),
    '$.items[*]' columns( 
@@ -27,17 +27,17 @@ apex_web_service.make_rest_request(
 );
 
 
-https://g0a16906b023bfa-odcecmi.adb.eu-frankfurt-1.oraclecloudapps.com/ords/usrx00/d16_vendita/?limit=200
+https://g0a16906b023bfa-odcecmi.adb.eu-frankfurt-1.oraclecloudapps.com/ords/usrx00/d17_vendita/?limit=200
 
-https://g0a16906b023bfa-odcecmi.adb.eu-frankfurt-1.oraclecloudapps.com/ords/usrx00/d16_vendita/?offset=50
+https://g0a16906b023bfa-odcecmi.adb.eu-frankfurt-1.oraclecloudapps.com/ords/usrx00/d17_vendita/?offset=50
 
-https://g0a16906b023bfa-odcecmi.adb.eu-frankfurt-1.oraclecloudapps.com/ords/usrx00/d16_vendita/?q={"Prodotto":{"$instr":"cotto"}}
+https://g0a16906b023bfa-odcecmi.adb.eu-frankfurt-1.oraclecloudapps.com/ords/usrx00/d17_vendita/?q={"Prodotto":{"$instr":"cotto"}}
 
-https://g0a16906b023bfa-odcecmi.adb.eu-frankfurt-1.oraclecloudapps.com/ords/usrx00/d16_vendita/?q={"importo":{"$lte":100}}
+https://g0a16906b023bfa-odcecmi.adb.eu-frankfurt-1.oraclecloudapps.com/ords/usrx00/d17_vendita/?q={"importo":{"$lte":100}}
 
-https://g0a16906b023bfa-odcecmi.adb.eu-frankfurt-1.oraclecloudapps.com/ords/usrx00/d16_vendita/?q={"data":{"$lte":{"$date":"2021-03-08T23:59:59Z"}}}
+https://g0a16906b023bfa-odcecmi.adb.eu-frankfurt-1.oraclecloudapps.com/ords/usrx00/d17_vendita/?q={"data":{"$lte":{"$date":"2021-03-08T23:59:59Z"}}}
 
-https://g0a16906b023bfa-odcecmi.adb.eu-frankfurt-1.oraclecloudapps.com/ords/usrx00/d16_vendita/?q={"importo": {"$and": [{"$gt": 100},{"$lt":300}]}}
+https://g0a16906b023bfa-odcecmi.adb.eu-frankfurt-1.oraclecloudapps.com/ords/usrx00/d17_vendita/?q={"importo": {"$and": [{"$gt": 100},{"$lt":300}]}}
 
 -- documentazione filtri
 https://docs.oracle.com/en/database/oracle/oracle-rest-data-services/21.4/aelig/developing-REST-applications.html#GUID-091748F8-3D14-402B-9310-25E6A9116B47
