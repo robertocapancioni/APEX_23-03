d8ab25d25b92657379b3215ff8f5cbdf038eebd2
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-05-23 07:16:38 +0200

    Update d18_merge.sql

--- D18/d18_merge.sql
+++ D18/d18_merge.sql
@@ -31,9 +31,6 @@ merge into d18_vendita d
                ) 
     when matched then
           update set
-                 --d.data         = s.data,
-                 --d.prodotto     = s.prodotto,
-                 --d.cliente      = s.cliente,
                  d.zona_cliente = s.zona_cliente,
                  d.quantita     = s.quantita,
                  d.importo      = s.importo
@@ -49,9 +46,6 @@ merge into d18_vendita d
                )
     when matched then
           update set
-                 --d.data         = s.data,
-                 --d.prodotto     = s.prodotto,
-                 --d.cliente      = s.cliente,
                  d.zona_cliente = s.zona_cliente,
                  d.quantita     = s.quantita,
                  d.importo      = s.importo
@@ -61,3 +55,20 @@ merge into d18_vendita d
           insert (d.data,d.prodotto,d.cliente,d.zona_cliente,d.quantita,d.importo)
           values (s.data,s.prodotto,s.cliente,s.zona_cliente,s.quantita,s.importo)
            where s.importo < 100;
+
+    merge into d18_vendita d
+         using d17_vendita s
+            on (    s.data     = d.data 
+                and s.prodotto = d.prodotto
+                and s.cliente  = d.cliente
+               )
+    when matched then
+          update set
+                 d.zona_cliente = s.zona_cliente,
+                 d.quantita     = s.quantita,
+                 d.importo      = s.importo
+           where d.importo < 100
+    when not matched then
+          insert (d.data,d.prodotto,d.cliente,d.zona_cliente,d.quantita,d.importo)
+          values (s.data,s.prodotto,s.cliente,s.zona_cliente,s.quantita,s.importo);
+
