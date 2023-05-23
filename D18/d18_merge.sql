5f02384cd891b4f2f776da988da89ca04898f88f
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-05-23 07:21:15 +0200

    Update d18_merge.sql

--- D18/d18_merge.sql
+++ D18/d18_merge.sql
@@ -71,4 +71,23 @@ merge into d18_vendita d
     when not matched then
           insert (d.data,d.prodotto,d.cliente,d.zona_cliente,d.quantita,d.importo)
           values (s.data,s.prodotto,s.cliente,s.zona_cliente,s.quantita,s.importo);
+          
+    merge into d18_vendita d
+         using d17_vendita s
+            on (    s.data     = d.data 
+                and s.prodotto = d.prodotto
+                and s.cliente  = d.cliente
+               )
+    when matched then
+          update set
+                 d.zona_cliente = s.zona_cliente,
+                 d.quantita     = s.quantita,
+                 d.importo      = s.importo
+           where d.importo < 100
+    delete where d.importo > 900
+    when not matched then
+          insert (d.data,d.prodotto,d.cliente,d.zona_cliente,d.quantita,d.importo)
+          values (s.data,s.prodotto,s.cliente,s.zona_cliente,s.quantita,s.importo)
+           where s.importo < 100;
+
 
