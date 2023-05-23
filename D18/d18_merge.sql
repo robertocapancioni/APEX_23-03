cb23a6bca79c1eb792ed4cb6f8960b877d1f06e8
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-05-23 07:04:23 +0200

    Update d18_merge.sql

--- D18/d18_merge.sql
+++ D18/d18_merge.sql
@@ -15,7 +15,32 @@ merge into d18_vendita d
     when not matched then
           insert (d.data,d.prodotto,d.cliente,d.zona_cliente,d.quantita,d.importo)
           values (s.data,s.prodotto,s.cliente,s.zona_cliente,s.quantita,s.importo);
-          
+  
+ merge into d18_vendita d
+         using (select  
+                 trunc(sysdate) data,
+                         'Pane' prodotto,
+                  'Mario Rossi' cliente,
+                         'NORD' zona_cliente,
+                            100 quantita,
+                            400 importo 
+                           from dual) s
+            on (    s.data     = d.data 
+                and s.prodotto = d.prodotto
+                and s.cliente  = d.cliente
+               ) 
+    when matched then
+          update set
+                 --d.data         = s.data,
+                 --d.prodotto     = s.prodotto,
+                 --d.cliente      = s.cliente,
+                 d.zona_cliente = s.zona_cliente,
+                 d.quantita     = s.quantita,
+                 d.importo      = s.importo
+    when not matched then
+          insert (d.data,d.prodotto,d.cliente,d.zona_cliente,d.quantita,d.importo)
+          values (s.data,s.prodotto,s.cliente,s.zona_cliente,s.quantita,s.importo);
+
     merge into d18_vendita d
          using d17_vendita s
             on (    s.data     = d.data 
