28e7a5477d0130d6bf60350f554569d3f1d12689
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-05-27 11:52:43 +0200

    Update 10-RICH_azione_approva.sql

--- D20/10-RICH_azione_approva.sql
+++ D20/10-RICH_azione_approva.sql
@@ -1,10 +1,15 @@
 declare
-l_stato_da_id d20_documento_dettaglio.stato_da_id%type;
+l_stato_a_id d20_documento_dettaglio.stato_a_id%type;
 l_stato d20_stato.stato%type :='10-RICH';
 l_richiedente d20_documento.richiedente%type := :APP_USER;
 begin
-:RICHEDENTE := l_richiedente;
-:STATO      := l_stato;
-select id into l_stato_da_id from d20_stato where stato =l_stato;
-insert into d20_documento_dettaglio (documento_id,data,utente,stato_da_id,stato_a_id) values (:APEX$TASK_PK,sysdate,l_richiedente,l_stato_da_id,l_stato_da_id);
+select id into l_stato_a_id from d20_stato where stato =l_stato;
+
+update d20_documento 
+   set richiedente = l_richiedente, 
+             stato_id = l_stato_a_id 
+ where id = :APEX$TASK_PK;
+
+insert into     d20_documento_dettaglio (documento_id,data,utente,stato_da_id,stato_a_id) 
+       values (:APEX$TASK_PK,sysdate,l_richiedente,l_stato_a_id,l_stato_a_id);
 end;
