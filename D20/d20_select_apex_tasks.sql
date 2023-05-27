8ed36e109fe7206b482ed47e68c3e5d4c5200b7c
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-05-27 16:25:06 +0200

    Create d20_select_apex_tasks.sql

---
+++ D20/d20_select_apex_tasks.sql
@@ -0,0 +1,13 @@
+select * from APEX_APPL_TASKDEFS             where application_id = 785
+select * from APEX_APPL_TASKDEF_ACTIONS      where application_id = 785
+select * from APEX_APPL_TASKDEF_PARAMS       where application_id = 785
+select * from APEX_APPL_TASKDEF_PARTICIPANTS where application_id = 785
+
+select * from APEX_APPL_TASKDEF_COMP_PARAMS  where application_id = 785
+
+
+select * from APEX_TASKS                     where application_id = 785
+select * from APEX_TASK_HISTORY              where application_id = 785
+select * from APEX_TASK_PARAMETERS           where application_id = 785
+select * from APEX_TASK_COMMENTS             where application_id = 785
+select * from APEX_TASK_PARTICIPANTS         where application_id = 785
