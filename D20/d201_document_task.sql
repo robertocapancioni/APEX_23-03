d1c6112dd280b816ca7509c129ea4389e28b89d0
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-05-28 11:44:26 +0200

    Create d201_document_task.sql

---
+++ D20/d201_document_task.sql
@@ -0,0 +1,8 @@
+select d.*,
+       a.*
+  from apex_tasks t
+  join table (apex_approval.get_tasks ('INITIATED_BY_ME'))a
+    on t.task_id = a.task_id and t.application_id = a.app_id
+  join d201_documento d 
+    on t.detail_pk = d.id
+ where a.app_id = :APP_ID
