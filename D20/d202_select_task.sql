98267519a7bd9ed781819d72540dad8b9cd3e810
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-05-28 16:11:53 +0200

    Update and rename d201_select_task.sql to d202_select_task.sql

--- D20/d202_select_task.sql
+++ D20/d202_select_task.sql
@@ -19,6 +19,24 @@ select t.task_id,
   from APEX_TASK_HISTORY h
   join APEX_TASKS t on h.task_id = t.task_id
                    and h.application_id = t.application_id
- where t.application_id = 786 and t.detail_pk = 1
+ where t.application_id = :APP_ID and t.detail_pk = :PX_ID
    and h.event_type_code = 'COMPLETE'
 order by h.event_timestamp 
+
+select t.task_id,
+       t.application_id,
+       t.task_def_name,
+       t.task_def_static_id,
+       t.detail_pk,
+       t.subject,
+       t.initiator,
+       t.actual_owner,
+       t.state,
+       t.outcome,
+       c.text,
+       c.created_on
+  from APEX_TASK_COMMENTS c
+  join APEX_TASKS t on c.task_id = t.task_id
+                   and c.application_id = t.application_id
+ where t.application_id = :APP_ID and t.detail_pk = :PX_ID
+ order by c.created_on
