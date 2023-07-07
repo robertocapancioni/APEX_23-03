ef9376322eb4d7e4d9b687878e15201602a8ba5a
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-07-07 13:06:00 +0200

    Update d08_prodotto_vw.sql

--- D08/d08_prodotto_vw.sql
+++ D08/d08_prodotto_vw.sql
@@ -1,10 +1,10 @@
-create or replace view d14_prodotto_vw as
+create or replace view d08_prodotto_vw as
 select 
        p.id,
       tp.tipo_prodotto,
        p.prodotto,
        p.prezzo_acquisto,
        p.prezzo_vendita
-  from d14_prodotto p
-  join d14_tipo_prodotto tp
+  from d08_prodotto p
+  join d08_tipo_prodotto tp
     on p.tipo_prodotto_id = tp.id
